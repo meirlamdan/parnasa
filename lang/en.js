007c08be5eadcfff1cae490453b04e0b89f046c7
meir lerner <meir3252@gmail.com>
2024-08-12 12:45:59 +0300

    translations

---
+++ lang/en.js
@@ -0,0 +1,40 @@
+export default {
+  links: {
+    home: 'Home',
+    about: 'About',
+    contact: 'Contact',
+    jobs: 'Our Jobs'
+  },
+
+  main: {
+    title: 'Parnassah B’heter Uv’chavod',
+    content: 'Earning a respectable livelihood, in accordance with halachah, lorm ipsum dolar sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam'
+  },
+  about: {
+    title: 'About Parnasa',
+    part1: {
+      title: 'What is “Parnassah”?',
+      content: `The Parnassah – B’heter Uv’chavod organization was established in Eretz Yisrael to help those avreichim who need to go to work to support their families, and to guide them so that they can find employment that is appropriate for them. “Parnassah” helps them find the place that is best for them, based on their particular talents and abilities. We guide the avreichim from the moment they contact us until, b’ezras Hashem, they find a job that is just right for them and they begin working. Afterward as well, we help them advance in their jobs, so that they are using their talents to achieve their full potential, earning a livelihood in a dignified manner and fully in accordance with halachah.
+
+The Parnassah organization matches businesses that seek employees with avreichim who are seeking jobs.
+` },
+    part2: {
+      title: 'Parnassah’s activities',
+      content: `Over the past year, Parnassah assisted and guided over 200 heads of families, who are today earning respectable livelihoods in accordance with halachah. The lives of more than 1,000 children have been changed for the better because their fathers now have good jobs. When a family lacks parnassah, it is in real crisis. People feel comfortable when they know that they can eat well and dress in nice clothing. Avreichim tell us that before they had good jobs their households suffered from the stress of poverty, and that led to all kinds of problems, chas v’shalom. Once Parnassah helped them to become self-sufficient, b’ezras Hashem, they were able to manage their finances, the atmosphere in their homes became joyful, and they are now able to bring up their children calmly and happily.`
+    },
+    part3: {
+      title: 'Who Stands Behind Parnassah',
+      content: `Parnassah – B’heter Uv’chavod is the initiative of Rabbanim from across the chareidi sector, who were concerned that financial stress affects so many chareidi families in Eretz Yisrael¸ leading to deteriorating shalom bayis, problems relating to the chinuch of their children, and more. <br
+
+Parnassah – B’heter Uv’chavod enjoys the enthusiastic blessings and support of all the gedolei hador, shlita, who consider this organization to be a practical boon for our generation. They have described the organization as “this generation’s shield,” safeguarding the material and spiritual status of Jewish homes.`
+    }
+  },
+  form: {
+    name: 'Name',
+    email: 'Email',
+    message: 'Message',
+    submit: 'Send',
+    success: 'Thank you for your message, we will get back to you shortly.',
+    error: 'An error occurred while sending your message. Please try again later.',
+  }
+}
